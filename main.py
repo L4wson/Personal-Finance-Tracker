f3cbac4b9fd0428f536ab87a8f778ece9b377314
L4wson <84114068+L4wson@users.noreply.github.com>
2024-08-02 14:33:45 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello Worlds")
+import tkinter as tk
+from tkinter import ttk
+
+
+def add_income():
+    pass
+
+def add_expense():
+    pass
+
+root = tk.Tk()
+root.title("Personal Finance Manager")
