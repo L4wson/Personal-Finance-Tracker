5ecfff8ea508f4a79a3938979548a392ef1f6847
L4wson <84114068+L4wson@users.noreply.github.com>
2024-09-02 13:05:45 -0700

    finishing database.py

--- database.py
+++ database.py
@@ -9,10 +9,18 @@ def create_db():
     conn.close()
 
 def add_income():
-    pass
+    conn = sqlite3.connect('finance.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO income (...) Values (...)", (...))
+    conn.commit()
+    conn.close()
 
 def add_expense():
-    pass
+    conn = sqlite3.connect('finance.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO expense (...) Values (...)", (...))
+    conn.commit()
+    conn.close()
 
 def fetch_data():
     conn = sqlite3.connect('finance.db')
